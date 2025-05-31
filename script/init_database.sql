312a83bc79a65786eb631d88b0bc89643cff2111
Joshna004 <joshnaeee@gmail.com>
2025-05-31 09:44:09 +0100

    Update and rename init_db to init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,14 +1,14 @@
 ================================
 #create databse
 #drop and recreate database
-IF EXISTS (SELECT 1 from sys.databases WHERE name='Datawarehouse')
+IF EXISTS (SELECT 1 from sys.databases WHERE name='Datawarehouse_new')
 BEGIN
-ALTER DATABASE Datawarehouse SET single_user WITH ROLLBACK IMMEDIATE;
-DROP DATABASE DATAWAREHOUSE;
+ALTER DATABASE Datawarehouse_new SET single_user WITH ROLLBACK IMMEDIATE;
+DROP DATABASE DATAWAREHOUSE_new;
 END;
 go
 ---Create the database 'datawarehouse_new
-CREATE DATABASE Datawarehouse;
+CREATE DATABASE Datawarehouse_new;
 go
 USE DATAWAREHOUSE_NEW;
 create schema bronze;
